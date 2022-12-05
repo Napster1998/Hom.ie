466c854f40df3781f093bab564932588a32935a2
Napster1998 <46029472+Napster1998@users.noreply.github.com>
2022-12-05 03:37:56 +0000

    names to listingPage and accepted values via POST

--- DJango/Homie/Homie/views.py
+++ DJango/Homie/Homie/views.py
@@ -6,4 +6,40 @@ from django.shortcuts import render
 "Listing Page Triggered"
 
 def goToListingPage(request):
+
+    if request.method=="POST":
+
+        fName = request.POST.get('firstName')
+        contact = request.POST.get('contact')
+        emailId = request.POST.get('email')
+        eir = request.POST.get('EIR')
+        availableFrom = request.POST.get('availableFromDate')
+        availableTo = request.POST.get('availableTillDate')
+        address = request.POST.get('fullAddress')
+        rent = request.POST.get('expectedRent')
+        deposite = request.POST.get('expectedDeposite')
+        includingBill = request.POST.get('billcheck')
+        noOfBedrooms = request.POST.get('numberOfBedrooms')
+        noOfBathrooms = request.POST.get('numberofBathrooms')
+        ensuiteNumber = request.POST.get('ensuiteNumber')
+        singleBed = request.POST.get('singleBed')
+        twinShare = request.POST.get('twinShare')
+        singleRoom = request.POST.get('singleRoom')
+        malePref = request.POST.get('malePref')
+        femalePref = request.POST.get('femalePref')
+        couplePref = request.POST.get('couplePref')
+        studentPref = request.POST.get('studentPref')
+        workingPref = request.POST.get('workingPref')
+        tvAvailable = request.POST.get('tvAvailable')
+        wifiAvailable = request.POST.get('wifiAvailable')
+        washingAvailable = request.POST.get('washingAvailable')
+        tableAvailable = request.POST.get('tableAvailable')
+        wardrobeAvailable = request.POST.get('wardrobeAvailable')
+        additionalTextOnListing = request.POST.get('additionalInfo')
+
+
+        print(fName,contact,emailId,eir,availableFrom,availableTo,address,rent,deposite,includingBill,noOfBedrooms,noOfBathrooms,ensuiteNumber,
+        singleBed,twinShare,singleRoom,malePref,femalePref,couplePref,studentPref,workingPref,tvAvailable,wifiAvailable,washingAvailable,
+        tableAvailable,wardrobeAvailable,additionalTextOnListing)
+
     return render(request,'listingPage.html')
