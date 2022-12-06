d864e38c17667af44114c8a86ef9bd178af96136
Napster1998 <46029472+Napster1998@users.noreply.github.com>
2022-12-06 18:09:03 +0000

    Able to now get data from frontend to DB

--- DJango/Homie/Homie/models.py
+++ DJango/Homie/Homie/models.py
@@ -1,9 +1,35 @@
 from django.db import models
-from . import views
+
 
 
 class listingRaw(models.Model):
-    listing_name = models.CharField(max_length=30)
-    listing_contact = models.IntegerField()
+    listing_name = models.CharField(max_length=30,default="")
+    listing_contact = models.CharField(max_length=15,default="")
+    listing_email = models.CharField(max_length=30,default="")
+    listing_eir = models.CharField(max_length=30,default="")
+    listing_available_from = models.CharField(max_length=10,default="")
+    listing_available_to = models.CharField(max_length=10,default="")
+    listing_address = models.CharField(max_length=50,default="")
+    listing_rent = models.IntegerField(default=0)
+    listing_deposite = models.IntegerField(default=0)
     listing_is_bill_included = models.BooleanField(null = True , default= False)
-    
+    listing_no_of_bedrooms = models.IntegerField(default=0)
+    listing_no_of_bathrooms = models.IntegerField(default=0)
+    listing_ensuite_no = models.IntegerField(default=0)
+    listing_single_bed = models.BooleanField(null = True , default= False)
+    listing_twin_share = models.BooleanField(null = True , default= False)
+    listing_single_room = models.BooleanField(null = True , default= False)
+    listing_male_preferred = models.BooleanField(default = 0, null = 1)
+    listing_female_preferred = models.BooleanField(null = True , default= False)
+    listing_couple_preferred = models.BooleanField(null = True , default= False)
+    listing_student_preferred = models.BooleanField(null = True , default= False)
+    listing_working_preferred = models.BooleanField(null = True , default= False)
+    listing_tv_available = models.BooleanField(null = True , default= False)
+    listing_wifi_available = models.BooleanField(null = True , default= False)
+    listing_washing_available = models.BooleanField(null = True , default= False)
+    listing_table_available = models.BooleanField(null = True , default= False)
+    listing_wardrobe_available = models.BooleanField(null = True , default= False)
+    listing_additional_text = models.CharField(max_length=300, default="")
+
+
+
--- DJango/Homie/Homie/views.py
+++ DJango/Homie/Homie/views.py
@@ -2,7 +2,7 @@ from contextlib import _RedirectStream
 from django.http import HttpResponse
 from django.shortcuts import *
 from django.http import HttpResponseRedirect
-
+from .models import listingRaw
 
 "Listing Page Triggered"
 
@@ -38,9 +38,43 @@ def goToListingPage(request):
         wardrobeAvailable = request.POST.get('wardrobeAvailable')
         additionalTextOnListing = request.POST.get('additionalInfo')
 
-        print(fName,contact,emailId,eir,availableFrom,availableTo,address,rent,deposite,includingBill,noOfBedrooms,noOfBathrooms,ensuiteNumber,
+        rawListing = [fName,contact,emailId,eir,availableFrom,availableTo,address,rent,deposite,includingBill,noOfBedrooms,noOfBathrooms,ensuiteNumber,
         singleBed,twinShare,singleRoom,malePref,femalePref,couplePref,studentPref,workingPref,tvAvailable,wifiAvailable,washingAvailable,
-        tableAvailable,wardrobeAvailable,additionalTextOnListing)
+        tableAvailable,wardrobeAvailable,additionalTextOnListing]
+
+        print(noOfBedrooms,noOfBathrooms,malePref,femalePref,couplePref)
+
+        rawlist = listingRaw(
+            listing_name = fName, 
+            listing_contact = contact, 
+            listing_email = emailId, 
+            listing_eir = eir, 
+            listing_available_from = availableFrom, 
+            listing_available_to = availableTo,
+            listing_address = address, 
+            listing_rent = rent, 
+            listing_deposite = deposite,
+            listing_is_bill_included = includingBill, 
+            listing_no_of_bedrooms = noOfBedrooms, 
+            listing_no_of_bathrooms = noOfBathrooms,
+            listing_ensuite_no = ensuiteNumber,
+            listing_single_bed = singleBed, 
+            listing_twin_share = twinShare, 
+            listing_single_room = singleRoom, 
+            listing_male_preferred = malePref, 
+            listing_female_preferred = femalePref, 
+            listing_couple_preferred = couplePref, 
+            listing_student_preferred = studentPref,
+            listing_working_preferred = workingPref, 
+            listing_tv_available = tvAvailable, 
+            listing_wifi_available = wifiAvailable, 
+            listing_washing_available = washingAvailable,
+            listing_table_available = tableAvailable,
+            listing_wardrobe_available = wardrobeAvailable,
+            listing_additional_text = additionalTextOnListing
+            )
+
+        rawlist.save()
 
         return redirect('/landingPage')
 
